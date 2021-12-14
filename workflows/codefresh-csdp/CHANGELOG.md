f54ba495a157ebc650c63b16ddefe8d818a8ec23
vadim-kharin-codefresh <vadim.kharin@codefresh.io>
2021-12-14 15:42:23 +0200

    Add codefresh csdp workflow with image enricher template (#12)
    Co-authored-by: itai-codefresh <itai@codefresh.io>

---
+++ workflows/codefresh-csdp/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## v0.0.1 (14.12.2021)
+
+### image-enricher-git
+
+Enrich codefresh image with PRs and Issues info. Adds pull request information to an image.
---
+++ workflows/codefresh-csdp/versions/0.0.1/images/image-enricher-git/src/codefresh.api.js
@@ -0,0 +1,105 @@
+const { host, apiToken, image, branch } = require('./configuration');
+const rp = require('request-promise');
+const _ = require('lodash');
+const chalk = require('chalk');
+
+class CodefreshAPI {
+    _handleError(e) {
+        if (_.get(e, 'error.message')) {
+            const code = _.get(e, 'error.code')
+            const statusCode = _.get(e, 'error.status')
+            const message = _.get(e, 'error.message')
+            throw new Error(`Codefresh error ${statusCode} [${code}]: ${message}`)
+        }
+
+        throw e
+    }
+
+    async getContext(name) {
+        try {
+            return await rp({
+                method: 'GET',
+                uri: `${host}/api/contexts/${name}?decrypt=true`,
+                headers: {
+                    'Authorization': `Bearer ${apiToken}`
+                },
+                json: true
+            });
+        } catch (e) {
+            return this._handleError(e);
+        }
+    }
+
+    async createPullRequestV2(pullRequest) {
+        console.log(chalk.green(`Create pull request ${pullRequest.number}=${pullRequest.url}, image ${image}`));
+        try {
+            const body = {
+                "operationName":"saveAnnotation",
+                "variables":{
+                    "annotation":{
+                        "logicEntityId": {"id": image},
+                        "entityType":"image",
+                        "key": `#${pullRequest.number}`,
+                        "type": "pr",
+                        "pullRequestValue": {
+                            url: pullRequest.url,
+                            title: pullRequest.title,
+                            committers: pullRequest.committers
+                        }
+                    }
+                },
+                "query":"mutation saveAnnotation( $annotation: AnnotationArgs!) {\n saveAnnotation(annotation: $annotation)\n}"
+            }
+            return await rp({
+                method: 'POST',
+                uri: `${host}/2.0/api/graphql`,
+                body,
+                headers: {
+                    'Authorization': `Bearer ${apiToken}`
+                },
+                json: true
+            });
+        } catch (e) {
+            return this._handleError(e);
+        }
+
+    }
+
+    async createPullRequest(pullRequest) {
+
+        console.log(chalk.green(`Create pull request ${pullRequest.number}=${pullRequest.url}, image ${image}`));
+
+        try {
+            return await rp({
+                method: 'POST',
+                uri: `${host}/api/annotations`,
+                body: {
+                    entityId: image,
+                    entityType: 'image-prs',
+                    key: `#${pullRequest.number}`,
+                    value: {
+                        url: pullRequest.url,
+                        title: pullRequest.title,
+                        committers: pullRequest.committers
+                    },
+                    bigValue: {
+                        firstCommitDate: pullRequest.firstCommitDate,
+                        lastCommitDate: pullRequest.lastCommitDate,
+                        prDate: pullRequest.prDate,
+                        branch,
+                        commits: pullRequest.commits,
+                    }
+                },
+                headers: {
+                    'Authorization': `Bearer ${apiToken}`
+                },
+                json: true
+            });
+        } catch (e) {
+            return this._handleError(e);
+        }
+    }
+
+}
+
+module.exports = new CodefreshAPI();
