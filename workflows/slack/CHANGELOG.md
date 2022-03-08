7d782441b3476d49b86955eee1dae4a42c035abf
Eti Zaguri <eti.zaguri@codefresh.io>
2022-03-08 18:33:54 +0200

    Delete workflows/slack directory

--- workflows/slack/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-## v0.0.1 (1.12.2021)
-
-### send-message
-
-Send a message to a Slack channel using a hook url
--- workflows/slack/versions/0.0.1/images/send-message/index.js
+++
@@ -1,9 +0,0 @@
-
-const Mode = require('./src/mode');
-
-if (!process.env.SLACK_HOOK_URL) {
-    console.error('SLACK_HOOK_URL env variable should be present');
-    process.exit(1);
-}
-
-Mode.send(process.env.MODE);
