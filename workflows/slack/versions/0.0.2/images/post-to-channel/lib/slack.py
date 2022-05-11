c1b421327021b814783bdc6492186a1f7952c38a
Laurent Rochette <laurent.rochette@codefresh.io>
2022-05-11 11:52:40 -0700

    Merge into local branch first (#263)
    * fixed boolean value into a string
    
    * fixed boolean value into a boolean for DEBUG
    
    * draft
    
    * Repalce container by script
    
    * revert back t comtainer
    
    * Adding close CR
    
    * Force exit 0 on commit
    
    * fix syntax
    
    * Change extension to rpevent the sample auth to be included
    
    * Adding formal logging
    Changing DEBUG parameter to LOGLEVEL
    
    * Fix actio for close CR
    
    * slioghtly dsifferent debug info in closecr
    
    * minor editing fixes
    
    * Change image location to official one
    
    * Change annotations to include argo-hub in all
    
    * Revert to private image
    
    * Created a placeholder file in tree/main/workflows/servicenow Directory
    
    * remove dummy path
    
    * First version of plugin
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Fix mess between the 2 templates
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * fixes
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * missing quotes
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Misspelling
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Change image to official
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * bringing ServiceNow code from fork (#131) (#132)
    
    * fixed boolean value into a string
    
    * fixed boolean value into a boolean for DEBUG
    
    * draft
    
    * Repalce container by script
    
    * revert back t comtainer
    
    * Adding close CR
    
    * Force exit 0 on commit
    
    * fix syntax
    
    * Change extension to rpevent the sample auth to be included
    
    * Adding formal logging
    Changing DEBUG parameter to LOGLEVEL
    
    * Fix actio for close CR
    
    * slioghtly dsifferent debug info in closecr
    
    * minor editing fixes
    
    * Change image location to official one
    
    * Change annotations to include argo-hub in all
    
    * reverting to privtae image to continur testing
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Feedback from Eti
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>

---
+++ workflows/slack/versions/0.0.2/images/post-to-channel/lib/slack.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+'''
+Script to send a message to a named Slack channel
+'''
+
+import os
+import sys
+import logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+def main():
+    log_format = "%(asctime)s:%(levelname)s:%(name)s.%(funcName)s: %(message)s"
+    logging.basicConfig(format = log_format, level = os.environ['LOG_LEVEL'].upper())
+
+    channel=os.getenv('SLACK_CHANNEL')
+    message=os.getenv('SLACK_MESSAGE', "")
+    token  =os.getenv('SLACK_TOKEN')
+
+    if ( channel == None ):
+        logging.error("SLACK_CHANNEL is not defined")
+        sys.exit(1)
+
+    if ( token == None ):
+        logging.error("SLACK_TOKEN is not defined")
+        sys.exit(1)
+
+    logging.info("Connecting to Slack")
+    client = WebClient(token=token)
+
+    if ( not channel.startswith('@') and '@' in channel ):
+        try:
+            response = client.users_lookupByEmail(email=channel)
+            assert response["user"]["profile"]["email"] == channel
+            channel = response["user"]["id"]
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            assert e.response["ok"] is False
+            assert e.response["error"]  # str like 'users_not_found'
+            logging.error("Lookup error: %s",e.response['error'])
+            sys.exit(3)
+
+    try:
+        response = client.chat_postMessage(channel=channel, text=message)
+        assert response["message"]["text"] == message
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        logging.error("Post error: %s",e.response['error'])
+        sys.exit(2)
+    logging.info("Message posted to Slack")
+
+if __name__ == "__main__":
+    main()
