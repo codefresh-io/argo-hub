80b4e8a785aaaf2ffdd52cd48e41df24cd5e9351
Laurent Rochette <laurent.rochette@codefresh.io>
2022-05-12 13:55:34 -0700

    Merge into local branch first (#263) (#264)
    * fixed boolean value into a string
    
    * fixed boolean value into a boolean for DEBUG
    
    * draft
    
    * Repalce container by script
    
    * revert back t comtainer
    
    * Adding close CR
    
    * Force exit 0 on commit
    
    * fix syntax
    
    * Change extension to rpevent the sample auth to be included
    
    * Adding formal logging
    Changing DEBUG parameter to LOGLEVEL
    
    * Fix actio for close CR
    
    * slioghtly dsifferent debug info in closecr
    
    * minor editing fixes
    
    * Change image location to official one
    
    * Change annotations to include argo-hub in all
    
    * Revert to private image
    
    * Created a placeholder file in tree/main/workflows/servicenow Directory
    
    * remove dummy path
    
    * First version of plugin
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Fix mess between the 2 templates
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * fixes
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * missing quotes
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Misspelling
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Change image to official
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * bringing ServiceNow code from fork (#131) (#132)
    
    * fixed boolean value into a string
    
    * fixed boolean value into a boolean for DEBUG
    
    * draft
    
    * Repalce container by script
    
    * revert back t comtainer
    
    * Adding close CR
    
    * Force exit 0 on commit
    
    * fix syntax
    
    * Change extension to rpevent the sample auth to be included
    
    * Adding formal logging
    Changing DEBUG parameter to LOGLEVEL
    
    * Fix actio for close CR
    
    * slioghtly dsifferent debug info in closecr
    
    * minor editing fixes
    
    * Change image location to official one
    
    * Change annotations to include argo-hub in all
    
    * reverting to privtae image to continur testing
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    * Feedback from Eti
    
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>
    
    Co-authored-by: codefresh <eti.zaguri@codefresh.io>

---
+++ workflows/slack/versions/0.0.2/images/send-message/src/mode/simple.js
@@ -0,0 +1,22 @@
+
+const SlackApi = require('../slack-api');
+
+class SimpleMode {
+
+    static send() {
+        console.log('Choose simple mode');
+
+
+        const attachments = process.env.SLACK_ATTACHMENTS ? JSON.parse(process.env.SLACK_ATTACHMENTS) : undefined;
+
+        if (!process.env.SLACK_TEXT && !attachments) {
+            console.error('SLACK_TEXT or SLACK_ATTACHMENTS env variable should be present');
+            process.exit(1);
+        }
+
+        SlackApi.send(process.env.SLACK_TEXT, attachments, process.env.SLACK_USER_NAME, process.env.SLACK_ICON_EMOJI);
+    }
+
+}
+
+module.exports = SimpleMode;
