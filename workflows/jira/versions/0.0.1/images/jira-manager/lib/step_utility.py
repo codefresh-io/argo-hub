46ee67de47448c1ab6514d17d915f480d71f04d5
matthewchungcodefresh <106268971+matthewchungcodefresh@users.noreply.github.com>
2022-06-13 14:13:00 -0500

    Update output variables

--- workflows/jira/versions/0.0.1/images/jira-manager/lib/step_utility.py
+++ workflows/jira/versions/0.0.1/images/jira-manager/lib/step_utility.py
@@ -18,13 +18,8 @@ class StepUtility:
         sys.stderr.write('\x1b[1;31m' + message.strip() + '\x1b[0m' + end)
 
     @staticmethod
-    def export_variable(key, value):
-        # Export secrets here
-        print("\nExporting Codefresh variable")    
-        env_file_path = "/meta/env_vars_to_export"
-        if not os.path.exists(env_file_path):
-            print(str(key) + "=" + str(value))
-        else:
-            env_file = open(env_file_path, "a")           
-            env_file.write(str(key) + "=" + str(value) + "\n")
-            env_file.close()
+    def export_variable(name, value):
+        print(f"   {name}: {value}")
+        file=open(f"/tmp/{name}", "w")
+        file.write(f"{value}")
+        file.close()
