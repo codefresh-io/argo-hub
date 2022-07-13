2cec560efb1c6db00547a95bb8f3f59074716fb0
vadim-kharin-codefresh <vadim.kharin@codefresh.io>
2022-07-13 16:24:04 +0300

    Revert "Add bitbucket support to image-enricher-git-info plugin (#370)" (#375)
    This reverts commit 327370459076be0ab9f2b7dec73543d20a7feda6.

--- workflows/codefresh-csdp/versions/1.1.0/images/image-enricher-git-info/src/providers/bitbucket/bitbucker.service.js
+++ workflows/codefresh-csdp/versions/1.1.0/images/image-enricher-git-info/src/providers/bitbucket/bitbucker.service.js
@@ -1,45 +1,15 @@
-const _ = require('lodash');
-const bitBucketApi = require('./bitbucker.api');
+const chalk = require('chalk');
 
 class BitbucketService {
 
-    async getPullRequestsWithCommits(repo, branch) {
-        try {
-            const prs = await bitBucketApi.getPullRequests(repo, branch);
-            return Promise.all(prs.map(async (pr) => {
-                const info = await bitBucketApi.getCommitsInfo(repo, pr);
-                const result = {
-                    ...info,
-                    prDate: pr.updated_on || pr.created_on,
-                    number: pr.id,
-                    url: `${pr.links.html.href}`,
-                    title: pr.title,
-                }
-
-                return result;
-            }));
-        } catch (error) {
-            throw new Error(`failed to get pull requests: ${error.message}`)
-        }
+    async getPullRequestsWithCommits() {
+        console.log(chalk.yellow(`Bitbucket provider hasn't implemented yet`));
+        return [];
     }
 
-    async getBranch(repo, branch) {
-        try {
-            const bitBucketBranch = await bitBucketApi.getBranch(repo, branch)
-            return {
-                name: bitBucketBranch.name,
-                commit: bitBucketBranch.target.hash,
-                commitMsg: bitBucketBranch.target.message,
-                commitURL: bitBucketBranch.target.links.html.href,
-                committerUsername: bitBucketBranch.target.author.user.nickname
-            }
-        } catch (error) {
-            if (_.get(error, 'status') === 404) {
-                // branch could be delete, but PR still can be found
-                return
-            }
-            throw new Error(`failed to get branch: ${error.message}`)
-        }
+    async getBranch() {
+        console.log(chalk.yellow(`Bitbucket provider hasn't implemented yet`));
+        return null;
     }
 }
 module.exports = new BitbucketService();
