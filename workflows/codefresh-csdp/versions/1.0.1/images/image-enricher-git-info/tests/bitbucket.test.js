330e1f5ab42e85ffdcf445972deeebfb271ad729
mikhail-klimko <mikhail.klimko@codefresh.io>
2023-10-06 00:18:19 +0300

    feat: multarch (#528)

--- workflows/codefresh-csdp/versions/1.0.1/images/image-enricher-git-info/tests/bitbucket.test.js
+++
@@ -1,7 +0,0 @@
-const file = require('../src/pull-request/bitbucket/file');
-
-test('should be able retrieve prs from bitbucket event file', async () => {
-  const prs = await file.pullRequests();
-  const pr  = {"number":3,"title":"v4","url":"https://bitbucket.org/vadpasseka/iremember/pull-requests/3"};
-  expect(prs[0]).toStrictEqual(pr);
-});
